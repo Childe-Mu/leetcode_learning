6799675f7e31f3f19116e92bb3a073e44e300ea0
moon <Childe_Mu@163.com>
2020-07-21 10:43:24 +0800

    RocketMQ单机版demo

---
+++ src/main/java/com/moon/javase/_thread/YieldDemo.java
@@ -0,0 +1,22 @@
+package com.moon.javase._thread;
+
+/**
+ * yield() demo
+ */
+public class YieldDemo {
+    public static void main(String[] args) {
+        Runnable yieldTask = () -> {
+            for (int i = 0; i <= 10; i++) {
+                System.out.println(Thread.currentThread().getName() + " " + i);
+                if (i == 5) {
+                    Thread.yield();
+                }
+            }
+        };
+
+        Thread thread1 = new Thread(yieldTask, "A");
+        Thread thread2 = new Thread(yieldTask, "B");
+        thread1.start();
+        thread2.start();
+    }
+}
