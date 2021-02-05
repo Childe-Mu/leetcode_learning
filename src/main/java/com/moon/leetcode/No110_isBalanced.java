4313b7e0ee887a0890bab07501d0e58ec67aac41
moon <Childe_Mu@163.com>
2021-02-05 15:53:14 +0800

    110. 平衡二叉树
    Signed-off-by: moon <Childe_Mu@163.com>

---
+++ src/main/java/com/moon/leetcode/No110_isBalanced.java
@@ -0,0 +1,96 @@
+package com.moon.leetcode;
+
+/**
+ * 110. 平衡二叉树
+ * <p>
+ * 给定一个二叉树，判断它是否是高度平衡的二叉树。
+ * <p>
+ * 本题中，一棵高度平衡二叉树定义为：
+ * <p>
+ * 一个二叉树每个节点 的左右两个子树的高度差的绝对值不超过 1 。
+ * <p>
+ * 示例 1：
+ * <p>
+ * 输入：root = [3,9,20,null,null,15,7]
+ * 输出：true
+ * <p>
+ * 示例 2：
+ * <p>
+ * 输入：root = [1,2,2,3,3,null,null,4,4]
+ * 输出：false
+ * <p>
+ * 示例 3：
+ * <p>
+ * 输入：root = []
+ * 输出：true
+ * <p>
+ * 提示：
+ * <p>
+ * 树中的节点数在范围 [0, 5000] 内
+ * -104 <= Node.val <= 104
+ * <p>
+ * https://leetcode-cn.com/problems/balanced-binary-tree/
+ */
+public class No110_isBalanced {
+    /**
+     * 自顶向下
+     */
+    public boolean isBalanced(TreeNode root) {
+        if (root == null) {
+            return true;
+        } else {
+            return Math.abs(height(root.left) - height(root.right)) <= 1 && isBalanced(root.left) && isBalanced(root.right);
+        }
+    }
+
+    public int height(TreeNode root) {
+        if (root == null) {
+            return 0;
+        } else {
+            return Math.max(height(root.left), height(root.right)) + 1;
+        }
+    }
+
+    /**
+     * 自底向上
+     */
+    public boolean isBalanced_v2(TreeNode root) {
+        if (root == null) {
+            return true;
+        } else {
+            return height_v2(root) >= 0;
+        }
+    }
+
+    public int height_v2(TreeNode node) {
+        if (node == null) {
+            return 0;
+        }
+        int left = height_v2(node.left);
+        int right = height_v2(node.right);
+        if (left == -1 || right == -1 || Math.abs(left - right) > 1) {
+            return -1;
+        } else {
+            return Math.max(left, right) + 1;
+        }
+    }
+
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+}
