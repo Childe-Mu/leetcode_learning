e3def3950bda50e36318dadad44194f7be2b1dda
moon <Childe_Mu@163.com>
2021-02-20 19:34:05 +0800

    125. 验证回文串
    Signed-off-by: moon <Childe_Mu@163.com>

---
+++ src/main/java/com/moon/leetcode/No125_isPalindrome.java
@@ -0,0 +1,44 @@
+package com.moon.leetcode;
+
+/**
+ * 125. 验证回文串
+ * <p>
+ * 给定一个字符串，验证它是否是回文串，只考虑字母和数字字符，可以忽略字母的大小写。
+ * <p>
+ * 说明：本题中，我们将空字符串定义为有效的回文串。
+ * <p>
+ * 示例 1:
+ * <p>
+ * 输入: "A man, a plan, a canal: Panama"
+ * 输出: true
+ * <p>
+ * 示例 2:
+ * <p>
+ * 输入: "race a car"
+ * 输出: false
+ */
+public class No125_isPalindrome {
+    public static boolean isPalindrome(String s) {
+        int left = 0, right = s.length() - 1;
+        while (left < right) {
+            while (left < right && !Character.isLetterOrDigit(s.charAt(left))) {
+                left++;
+            }
+            while (left < right && !Character.isLetterOrDigit(s.charAt(right))) {
+                right--;
+            }
+            if (left < right) {
+                if (Character.toLowerCase(s.charAt(left)) != Character.toLowerCase(s.charAt(right))) {
+                    return false;
+                }
+                left++;
+                right--;
+            }
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(isPalindrome("A man, a plan, a canal: Panama"));
+    }
+}
