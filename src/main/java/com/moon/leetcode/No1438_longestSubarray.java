8ab1ca87fe4cd5067c133dd0265dbeb8581703bc
moon <Childe_Mu@163.com>
2021-02-22 15:18:22 +0800

    1438. 绝对差不超过限制的最长连续子数组
    Signed-off-by: moon <Childe_Mu@163.com>

---
+++ src/main/java/com/moon/leetcode/No1438_longestSubarray.java
@@ -0,0 +1,126 @@
+package com.moon.leetcode;
+
+import java.util.Deque;
+import java.util.LinkedList;
+import java.util.TreeMap;
+
+/**
+ * 1438. 绝对差不超过限制的最长连续子数组
+ * <p>
+ * 给你一个整数数组 nums ，和一个表示限制的整数 limit，请你返回最长连续子数组的长度，该子数组中的任意两个元素之间的绝对差必须小于或者等于 limit 。
+ * <p>
+ * 如果不存在满足条件的子数组，则返回 0 。
+ * <p>
+ * 示例 1：
+ * <p>
+ * 输入：nums = [8,2,4,7], limit = 4
+ * 输出：2
+ * 解释：所有子数组如下：
+ * [8] 最大绝对差 |8-8| = 0 <= 4.
+ * [8,2] 最大绝对差 |8-2| = 6 > 4.
+ * [8,2,4] 最大绝对差 |8-2| = 6 > 4.
+ * [8,2,4,7] 最大绝对差 |8-2| = 6 > 4.
+ * [2] 最大绝对差 |2-2| = 0 <= 4.
+ * [2,4] 最大绝对差 |2-4| = 2 <= 4.
+ * [2,4,7] 最大绝对差 |2-7| = 5 > 4.
+ * [4] 最大绝对差 |4-4| = 0 <= 4.
+ * [4,7] 最大绝对差 |4-7| = 3 <= 4.
+ * [7] 最大绝对差 |7-7| = 0 <= 4.
+ * 因此，满足题意的最长子数组的长度为 2 。
+ * 示例 2：
+ * <p>
+ * 输入：nums = [10,1,2,4,7,2], limit = 5
+ * 输出：4
+ * 解释：满足题意的最长子数组是 [2,4,7,2]，其最大绝对差 |2-7| = 5 <= 5 。
+ * 示例 3：
+ * <p>
+ * 输入：nums = [4,2,2,2,4,4,2,2], limit = 0
+ * 输出：3
+ * <p>
+ * 提示：
+ * <p>
+ * 1 <= nums.length <= 10^5
+ * 1 <= nums[i] <= 10^9
+ * 0 <= limit <= 10^9
+ */
+public class No1438_longestSubarray {
+
+    /**
+     * O(2nlogn)
+     */
+    public static int longestSubarray_v1(int[] nums, int limit) {
+        TreeMap<Integer, Integer> map = new TreeMap<>();
+        int n = nums.length;
+        int left = 0, right = 0;
+        int ret = 0;
+        while (right < n) {
+            map.put(nums[right], map.getOrDefault(nums[right], 0) + 1);
+            if (map.lastKey() - map.firstKey() > limit) {
+                map.put(nums[left], map.get(nums[left]) - 1);
+                if (map.get(nums[left]) == 0) {
+                    map.remove(nums[left]);
+                }
+                left++;
+            }
+            ret = Math.max(ret, right - left + 1);
+            right++;
+        }
+        return ret;
+    }
+
+    /**
+     * 优化v1, O(nlogn)
+     */
+    public static int longestSubarray_v2(int[] nums, int limit) {
+        TreeMap<Integer, Integer> map = new TreeMap<>();
+        int n = nums.length;
+        int left = 0, right = 0;
+        while (right < n) {
+            map.put(nums[right], map.getOrDefault(nums[right], 0) + 1);
+            if (map.lastKey() - map.firstKey() > limit) {
+                map.put(nums[left], map.get(nums[left]) - 1);
+                if (map.get(nums[left]) == 0) {
+                    map.remove(nums[left]);
+                }
+                left++;
+            }
+            right++;
+        }
+        return right - left;
+    }
+
+    public static int longestSubarray(int[] nums, int limit) {
+        Deque<Integer> queMax = new LinkedList<>();
+        Deque<Integer> queMin = new LinkedList<>();
+        int n = nums.length;
+        int left = 0, right = 0;
+        int ret = 0;
+        while (right < n) {
+            while (!queMax.isEmpty() && queMax.peekLast() < nums[right]) {
+                queMax.pollLast();
+            }
+            while (!queMin.isEmpty() && queMin.peekLast() > nums[right]) {
+                queMin.pollLast();
+            }
+            queMax.offerLast(nums[right]);
+            queMin.offerLast(nums[right]);
+            if (!queMax.isEmpty() && !queMin.isEmpty() && queMax.peekFirst() - queMin.peekFirst() > limit) {
+                if (nums[left] == queMin.peekFirst()) {
+                    queMin.pollFirst();
+                }
+                if (nums[left] == queMax.peekFirst()) {
+                    queMax.pollFirst();
+                }
+                left++;
+            }
+            ret = Math.max(ret, right - left + 1);
+            right++;
+        }
+//        return ret;
+        return right - left;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(longestSubarray(new int[]{10,1,2,4,7,2}, 5));
+    }
+}
