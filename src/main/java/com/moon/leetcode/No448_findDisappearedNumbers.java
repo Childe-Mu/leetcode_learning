cfdc6a90fb0e39b84876f59a9708c7d8bc6c627b
moon <Childe_Mu@163.com>
2021-02-23 17:17:14 +0800

    448. 找到所有数组中消失的数字
    Signed-off-by: moon <Childe_Mu@163.com>

---
+++ src/main/java/com/moon/leetcode/No448_findDisappearedNumbers.java
@@ -0,0 +1,55 @@
+package com.moon.leetcode;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+/**
+ * 448. 找到所有数组中消失的数字
+ * <p>
+ * 给定一个范围在  1 ≤ a[i] ≤ n ( n = 数组大小 ) 的 整型数组，数组中的元素一些出现了两次，另一些只出现一次。
+ * <p>
+ * 找到所有在 [1, n] 范围之间没有出现在数组中的数字。
+ * <p>
+ * 您能在不使用额外空间且时间复杂度为O(n)的情况下完成这个任务吗? 你可以假定返回的数组不算在额外空间内。
+ * <p>
+ * 示例:
+ * <p>
+ * 输入:
+ * [4,3,2,7,8,2,3,1]
+ * <p>
+ * 输出:
+ * [5,6]
+ */
+public class No448_findDisappearedNumbers {
+    public static List<Integer> findDisappearedNumbers(int[] nums) {
+        Set<Integer> set = new HashSet<>();
+        for (int i = 1; i <= nums.length; i++) {
+            set.add(i);
+        }
+        for (int num : nums) {
+            set.remove(num);
+        }
+        return new ArrayList<>(set);
+    }
+
+    public static List<Integer> findDisappearedNumbers_v2(int[] nums) {
+        int n = nums.length;
+        for (int num : nums) {
+            int x = (num - 1) % n;
+            nums[x] += n;
+        }
+        List<Integer> ret = new ArrayList<Integer>();
+        for (int i = 0; i < n; i++) {
+            if (nums[i] <= n) {
+                ret.add(i + 1);
+            }
+        }
+        return ret;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(findDisappearedNumbers_v2(new int[]{4,3,2,7,8,2,3,1}));
+    }
+}
