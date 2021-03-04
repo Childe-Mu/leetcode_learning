f9978df0ee2c116ac72a176223213248714c3a54
moon <Childe_Mu@163.com>
2021-03-04 21:26:14 +0800

    354. 俄罗斯套娃信封问题
    Signed-off-by: moon <Childe_Mu@163.com>

---
+++ src/main/java/com/moon/leetcode/No354_maxEnvelopes.java
@@ -0,0 +1,86 @@
+package com.moon.leetcode;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * 354. 俄罗斯套娃信封问题
+ * <p>
+ * 给定一些标记了宽度和高度的信封，宽度和高度以整数对形式 (w, h) 出现。当另一个信封的宽度和高度都比这个信封大的时候，这个信封就可以放进另一个信封里，如同俄罗斯套娃一样。
+ * <p>
+ * 请计算最多能有多少个信封能组成一组“俄罗斯套娃”信封（即可以把一个信封放到另一个信封里面）。
+ * <p>
+ * 说明:
+ * <p>
+ * 不允许旋转信封。
+ * <p>
+ * 示例:
+ * <p>
+ * 输入: envelopes = [[5,4],[6,4],[6,7],[2,3]]
+ * 输出: 3
+ * 解释: 最多信封的个数为 3, 组合为: [2,3] => [5,4] => [6,7]。
+ */
+public class No354_maxEnvelopes {
+    /**
+     * 动态规划
+     */
+    public int maxEnvelopes_v1(int[][] envelopes) {
+        if (envelopes.length == 0) {
+            return 0;
+        }
+        int n = envelopes.length;
+        Arrays.sort(envelopes, (e1, e2) -> e1[0] != e2[0] ? e1[0] - e2[0] : e2[1] - e1[1]);
+
+        int[] dp = new int[n];
+        Arrays.fill(dp, 1);
+        int ans = 0;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < i; j++) {
+                if (envelopes[i][1] > envelopes[j][1]) {
+                    dp[i] = Math.max(dp[i], dp[j] + 1);
+                }
+            }
+            ans = Math.max(ans, dp[i]);
+        }
+        return ans;
+    }
+
+    /**
+     * 动态规划+二分
+     */
+    public int maxEnvelopes_v2(int[][] envelopes) {
+        if (envelopes.length == 0) {
+            return 0;
+        }
+
+        int n = envelopes.length;
+        Arrays.sort(envelopes, (e1, e2) -> e1[0] != e2[0] ? e1[0] - e2[0] : e2[1] - e1[1]);
+
+        List<Integer> f = new ArrayList<>();
+        f.add(envelopes[0][1]);
+        for (int i = 1; i < n; ++i) {
+            int num = envelopes[i][1];
+            if (num > f.get(f.size() - 1)) {
+                f.add(num);
+            } else {
+                int index = binarySearch(f, num);
+                f.set(index, num);
+            }
+        }
+        return f.size();
+    }
+
+    public int binarySearch(List<Integer> f, int target) {
+        int low = 0, high = f.size() - 1;
+        while (low < high) {
+            int mid = (high - low) / 2 + low;
+            if (f.get(mid) < target) {
+                low = mid + 1;
+            } else {
+                high = mid;
+            }
+        }
+        return low;
+    }
+}
