d9a612ead8d6956fa92ae891001bbeab810e4d5d
moon <Childe_Mu@163.com>
2021-03-09 22:18:56 +0800

    202. 快乐数
    Signed-off-by: moon <Childe_Mu@163.com>

---
+++ src/main/java/com/moon/leetcode/No202_isHappy.java
@@ -0,0 +1,57 @@
+package com.moon.leetcode;
+
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * 202. 快乐数
+ * <p>
+ * 编写一个算法来判断一个数 n 是不是快乐数。
+ * <p>
+ * 「快乐数」定义为：
+ * <p>
+ * 对于一个正整数，每一次将该数替换为它每个位置上的数字的平方和。
+ * 然后重复这个过程直到这个数变为 1，也可能是 无限循环 但始终变不到 1。
+ * 如果 可以变为 1，那么这个数就是快乐数。
+ * <p>
+ * 如果 n 是快乐数就返回 true ；不是，则返回 false 。
+ * <p>
+ * 示例 1：
+ * <p>
+ * 输入：19
+ * 输出：true
+ * 解释：
+ * 12 + 92 = 82
+ * 82 + 22 = 68
+ * 62 + 82 = 100
+ * 12 + 02 + 02 = 1
+ * <p>
+ * 示例 2：
+ * <p>
+ * 输入：n = 2
+ * 输出：false
+ * <p>
+ * 提示：
+ * <p>
+ * 1 <= n <= 231 - 1
+ */
+public class No202_isHappy {
+    private int getNext(int n) {
+        int totalSum = 0;
+        while (n > 0) {
+            int d = n % 10;
+            n = n / 10;
+            totalSum += d * d;
+        }
+        return totalSum;
+    }
+
+    public boolean isHappy(int n) {
+        Set<Integer> seen = new HashSet<>();
+        while (n != 1 && !seen.contains(n)) {
+            seen.add(n);
+            n = getNext(n);
+        }
+        return n == 1;
+    }
+}
