ad9e0f8a5903061604b304e076e9270d259ae08b
moon <Childe_Mu@163.com>
2021-03-18 19:09:32 +0800

    92. 反转链表 II
    Signed-off-by: moon <Childe_Mu@163.com>

---
+++ src/main/java/com/moon/leetcode/No92_reverseBetween.java
@@ -0,0 +1,167 @@
+package com.moon.leetcode;
+
+import com.google.common.base.MoreObjects;
+
+import java.util.Deque;
+import java.util.LinkedList;
+import java.util.StringJoiner;
+
+/**
+ * 92. 反转链表 II
+ * <p>
+ * 给你单链表的头节点 head 和两个整数 left 和 right ，其中 left <= right 。请你反转从位置 left 到位置 right 的链
+ * 表节点，返回 反转后的链表 。
+ * <p>
+ * 示例 1：
+ * <p>
+ * 输入：head = [1,2,3,4,5], left = 2, right = 4
+ * 输出：[1,4,3,2,5]
+ * <p>
+ * 示例 2：
+ * <p>
+ * 输入：head = [5], left = 1, right = 1
+ * 输出：[5]
+ * <p>
+ * 提示：
+ * <p>
+ * 链表中节点数目为 n
+ * 1 <= n <= 500
+ * -500 <= Node.val <= 500
+ * 1 <= left <= right <= n
+ * <p>
+ * 进阶： 你可以使用一趟扫描完成反转吗？
+ */
+public class No92_reverseBetween {
+
+    public static ListNode reverseBetween_v1(ListNode head, int left, int right) {
+        if (left == right) {
+            return head;
+        }
+
+        Deque<ListNode> deque = new LinkedList<>();
+        ListNode prev = new ListNode(0, head);
+
+        ListNode cur = prev, t1, t2;
+
+        for (int i = 1; i < left; i++) {
+            cur = cur.next;
+        }
+        t1 = cur;
+        for (int i = left; i <= right; i++) {
+            cur = cur.next;
+            deque.push(cur);
+        }
+        t2 = cur.next;
+        if (!deque.isEmpty()) {
+            t1.next = deque.getFirst();
+            while (deque.size() > 1) {
+                deque.pollFirst().next = deque.getFirst();
+            }
+            deque.getFirst().next = t2;
+        }
+        return prev.next;
+    }
+
+    public static ListNode reverseBetween_v2(ListNode head, int left, int right) {
+        // 因为头节点有可能发生变化，使用虚拟头节点可以避免复杂的分类讨论
+        ListNode dummyNode = new ListNode(-1);
+        dummyNode.next = head;
+
+        ListNode pre = dummyNode;
+        // 第 1 步：从虚拟头节点走 left - 1 步，来到 left 节点的前一个节点
+        // 建议写在 for 循环里，语义清晰
+        for (int i = 0; i < left - 1; i++) {
+            pre = pre.next;
+        }
+
+        // 第 2 步：从 pre 再走 right - left + 1 步，来到 right 节点
+        ListNode rightNode = pre;
+        for (int i = 0; i < right - left + 1; i++) {
+            rightNode = rightNode.next;
+        }
+
+        // 第 3 步：切断出一个子链表（截取链表）
+        ListNode leftNode = pre.next;
+        ListNode curr = rightNode.next;
+
+        // 注意：切断链接
+        pre.next = null;
+        rightNode.next = null;
+
+        // 第 4 步：同第 206 题，反转链表的子区间
+        reverseLinkedList(leftNode);
+
+        // 第 5 步：接回到原来的链表中
+        pre.next = rightNode;
+        leftNode.next = curr;
+        return dummyNode.next;
+    }
+
+    private static void reverseLinkedList(ListNode head) {
+        // 也可以使用递归反转一个链表
+        ListNode pre = null;
+        ListNode cur = head;
+
+        while (cur != null) {
+            ListNode next = cur.next;
+            cur.next = pre;
+            pre = cur;
+            cur = next;
+        }
+    }
+
+    public static ListNode reverseBetween_v3(ListNode head, int left, int right) {
+        // 设置 dummyNode 是这一类问题的一般做法
+        ListNode dummyNode = new ListNode(-1);
+        dummyNode.next = head;
+        ListNode pre = dummyNode;
+        for (int i = 0; i < left - 1; i++) {
+            pre = pre.next;
+        }
+        ListNode cur = pre.next;
+        ListNode next;
+        for (int i = 0; i < right - left; i++) {
+            next = cur.next;
+            cur.next = next.next;
+            next.next = pre.next;
+            pre.next = next;
+        }
+        return dummyNode.next;
+    }
+
+
+    public static void main(String[] args) {
+        ListNode node = new ListNode(5, null);
+        ListNode head = new ListNode(0, null);
+        head.next = node;
+        for (int i = 4; i >= 1; i--) {
+            ListNode n = new ListNode(i, null);
+            n.next = head.next;
+            head.next = n;
+        }
+        reverseBetween_v3(head.next, 2, 4);
+    }
+
+
+    static class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode() {
+        }
+
+        ListNode(int val) {
+            this.val = val;
+        }
+
+        ListNode(int val, ListNode next) {
+            this.val = val;
+            this.next = next;
+        }
+
+        @Override
+        public String toString() {
+            return String.valueOf(val);
+        }
+    }
+}
