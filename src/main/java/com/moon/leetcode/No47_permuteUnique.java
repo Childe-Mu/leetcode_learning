2ebc32bf1ee25641b6ca8f91351ea7565e2c0480
moon <Childe_Mu@163.com>
2021-03-26 19:17:39 +0800

    47. 全排列 II 回溯标准解法
    Signed-off-by: moon <Childe_Mu@163.com>

---
+++ src/main/java/com/moon/leetcode/No47_permuteUnique.java
@@ -0,0 +1,78 @@
+package com.moon.leetcode;
+
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Deque;
+import java.util.List;
+
+/**
+ * 47. 全排列 II
+ * <p>
+ * 给定一个可包含重复数字的序列 nums ，按任意顺序 返回所有不重复的全排列。
+ * <p>
+ * 示例 1：
+ * <p>
+ * 输入：nums = [1,1,2]
+ * 输出：
+ * [[1,1,2],
+ * [1,2,1],
+ * [2,1,1]]
+ * <p>
+ * 示例 2：
+ * <p>
+ * 输入：nums = [1,2,3]
+ * 输出：[[1,2,3],[1,3,2],[2,1,3],[2,3,1],[3,1,2],[3,2,1]]
+ * <p>
+ * 提示：
+ * <p>
+ * 1 <= nums.length <= 8
+ * -10 <= nums[i] <= 10
+ */
+public class No47_permuteUnique {
+    public List<List<Integer>> permuteUnique(int[] nums) {
+        int n = nums.length;
+        List<List<Integer>> res = new ArrayList<>();
+        if (n == 0) {
+            return res;
+        }
+        Arrays.sort(nums);
+        Deque<Integer> path = new ArrayDeque<>();
+        boolean[] used = new boolean[n];
+        backtrack(nums, n, 0, used, path, res);
+        return res;
+    }
+
+    /**
+     * 回溯算法
+     *
+     * @param nums  要回溯的对象
+     * @param len   对象长度（要回溯的最大深度）
+     * @param depth 深度
+     * @param path  回溯过程经过的路径
+     * @param res   回溯结果收集
+     * @param used  使用过的元素状态标记
+     */
+    private void backtrack(int[] nums, int len, int depth, boolean[] used, Deque<Integer> path, List<List<Integer>> res) {
+        if (depth == len) {
+            res.add(new ArrayList<>(path));
+            return;
+        }
+        for (int i = 0; i < len; ++i) {
+            if (used[i]) {
+                continue;
+            }
+            // 剪枝条件：i > 0 是为了保证 nums[i - 1] 有意义
+            // 写 !used[i - 1] 是因为 nums[i - 1] 在深度优先遍历的过程中刚刚被撤销选择
+            if (i > 0 && nums[i] == nums[i - 1] && !used[i - 1]) {
+                continue;
+            }
+            path.addLast(nums[i]);
+            used[i] = true;
+            backtrack(nums, len, depth + 1, used, path, res);
+            // 回溯部分的代码，和 dfs 之前的代码是对称的
+            used[i] = false;
+            path.removeLast();
+        }
+    }
+}
