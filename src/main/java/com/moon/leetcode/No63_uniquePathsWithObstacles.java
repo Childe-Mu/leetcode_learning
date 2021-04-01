edb548b6d0a777cb226ae2a29355f569d3b686f4
moon <Childe_Mu@163.com>
2021-04-01 20:40:29 +0800

    63. 不同路径 II
    Signed-off-by: moon <Childe_Mu@163.com>

---
+++ src/main/java/com/moon/leetcode/No63_uniquePathsWithObstacles.java
@@ -0,0 +1,57 @@
+package com.moon.leetcode;
+
+/**
+ * 63. 不同路径 II
+ * <p>
+ * 一个机器人位于一个 m x n 网格的左上角 （起始点在下图中标记为“Start” ）。
+ * <p>
+ * 机器人每次只能向下或者向右移动一步。机器人试图达到网格的右下角（在下图中标记为“Finish”）。
+ * <p>
+ * 现在考虑网格中有障碍物。那么从左上角到右下角将会有多少条不同的路径？
+ * <p>
+ * 网格中的障碍物和空位置分别用 1 和 0 来表示。
+ * <p>
+ * 示例 1：
+ * <p>
+ * 输入：obstacleGrid = [[0,0,0],[0,1,0],[0,0,0]]
+ * 输出：2
+ * <p>
+ * 解释：
+ * 3x3 网格的正中间有一个障碍物。
+ * 从左上角到右下角一共有 2 条不同的路径：
+ * 1. 向右 -> 向右 -> 向下 -> 向下
+ * 2. 向下 -> 向下 -> 向右 -> 向右
+ * <p>
+ * 示例 2：
+ * <p>
+ * 输入：obstacleGrid = [[0,1],[0,0]]
+ * 输出：1
+ * <p>
+ * 提示：
+ * <p>
+ * m == obstacleGrid.length
+ * n == obstacleGrid[i].length
+ * 1 <= m, n <= 100
+ * obstacleGrid[i][j] 为 0 或 1
+ */
+public class No63_uniquePathsWithObstacles {
+    public static void main(String[] args) {
+        System.out.println(new No63_uniquePathsWithObstacles().uniquePathsWithObstacles(new int[][]{{0, 0, 0}}));
+    }
+
+    public int uniquePathsWithObstacles(int[][] obstacleGrid) {
+        if (obstacleGrid[0][0] == 1) {
+            return 0;
+        }
+        int m = obstacleGrid.length;
+        int n = obstacleGrid[0].length;
+        int[][] f = new int[m + 1][n + 1];
+        f[0][1] = 1;
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                f[i + 1][j + 1] = obstacleGrid[i][j] == 0 ? f[i + 1][j] + f[i][j + 1] : 0;
+            }
+        }
+        return f[m][n];
+    }
+}
