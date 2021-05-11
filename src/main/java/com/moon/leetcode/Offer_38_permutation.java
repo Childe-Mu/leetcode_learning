7b27c3222c40fcd70e17b38a5e5e2386df3ce859
moon <Childe_Mu@163.com>
2021-05-11 16:54:56 +0800

    剑指 Offer 38. 字符串的排列
    Signed-off-by: moon <Childe_Mu@163.com>

--- src/main/java/com/moon/leetcode/Offer_38_permutation.java
+++ src/main/java/com/moon/leetcode/Offer_38_permutation.java
@@ -4,6 +4,8 @@ import java.util.ArrayDeque;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Deque;
+import java.util.HashSet;
+import java.util.LinkedList;
 import java.util.List;
 
 // 剑指 Offer 38. 字符串的排列
@@ -26,18 +28,10 @@ public class Offer_38_permutation {
     private List<String> res;
     private boolean[] used;
 
-    public static void main(String[] args) {
-        System.out.println(Arrays.toString(new Offer_38_permutation().permutation("aac")));
-    }
+    char[] c;
 
-    public String[] permutation(String s) {
-        Deque<Character> path = new ArrayDeque<>();
-        res = new ArrayList<>();
-        used = new boolean[s.length()];
-        char[] chars = s.toCharArray();
-        Arrays.sort(chars);
-        backtrack(chars, s.length(), 0, path);
-        return res.toArray(new String[0]);
+    public static void main(String[] args) {
+        System.out.println(Arrays.toString(new Offer_38_permutation().permutation_v2("abc")));
     }
 
     private void backtrack(char[] chars, int n, int depth, Deque<Character> path) {
@@ -67,4 +61,46 @@ public class Offer_38_permutation {
         }
         return sb.toString();
     }
+
+    public String[] permutation_v1(String s) {
+        Deque<Character> path = new ArrayDeque<>();
+        res = new ArrayList<>();
+        used = new boolean[s.length()];
+        char[] chars = s.toCharArray();
+        Arrays.sort(chars);
+        backtrack(chars, s.length(), 0, path);
+        return res.toArray(new String[0]);
+    }
+
+    public String[] permutation_v2(String s) {
+        c = s.toCharArray();
+        res = new LinkedList<>();
+        dfs(0);
+        return res.toArray(new String[0]);
+    }
+
+    void dfs(int x) {
+        if (x == c.length - 1) {
+            res.add(String.valueOf(c));      // 添加排列方案
+            return;
+        }
+        HashSet<Character> set = new HashSet<>();
+        for (int i = x; i < c.length; i++) {
+            char e = c[i];
+            if (set.contains(e)) {
+                continue; // 重复，因此剪枝
+            }
+            set.add(e);
+            swap(i, x);                      // 交换，将 c[i] 固定在第 x 位
+            dfs(x + 1);                      // 开启固定第 x + 1 位字符
+            swap(i, x);                      // 恢复交换
+        }
+    }
+
+    void swap(int a, int b) {
+        char tmp = c[a];
+        c[a] = c[b];
+        c[b] = tmp;
+    }
+
 }
