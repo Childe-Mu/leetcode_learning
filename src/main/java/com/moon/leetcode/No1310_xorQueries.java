d569f8ac313dab9b621dcf34284c5350065b32ce
moon <Childe_Mu@163.com>
2021-05-12 11:07:40 +0800

    1310. 子数组异或查询
    Signed-off-by: moon <Childe_Mu@163.com>

---
+++ src/main/java/com/moon/leetcode/No1310_xorQueries.java
@@ -0,0 +1,66 @@
+package com.moon.leetcode;
+
+import java.util.Arrays;
+
+// 1310. 子数组异或查询
+//
+//有一个正整数数组 arr，现给你一个对应的查询数组 queries，其中 queries[i] = [Li, Ri]。
+//
+// 对于每个查询 i，请你计算从 Li 到 Ri 的 XOR 值（即 arr[Li] xor arr[Li+1] xor ... xor arr[Ri]）作为
+//本次查询的结果。
+//
+// 并返回一个包含给定查询 queries 所有结果的数组。
+//
+//
+//
+// 示例 1：
+//
+// 输入：arr = [1,3,4,8], queries = [[0,1],[1,2],[0,3],[3,3]]
+//输出：[2,7,14,8]
+//解释：
+//数组中元素的二进制表示形式是：
+//1 = 0001
+//3 = 0011
+//4 = 0100
+//8 = 1000
+//查询的 XOR 值为：
+//[0,1] = 1 xor 3 = 2
+//[1,2] = 3 xor 4 = 7
+//[0,3] = 1 xor 3 xor 4 xor 8 = 14
+//[3,3] = 8
+//
+// 示例 2：
+//
+// 输入：arr = [4,8,2,10], queries = [[2,3],[1,3],[0,0],[0,3]]
+//输出：[8,0,4,4]
+//
+// 提示：
+//
+// 1 <= arr.length <= 3 * 10^4
+// 1 <= arr[i] <= 10^9
+// 1 <= queries.length <= 3 * 10^4
+// queries[i].length == 2
+// 0 <= queries[i][0] <= queries[i][1] < arr.length
+//
+// Related Topics 位运算
+// 👍 79 👎 0
+public class No1310_xorQueries {
+    public static void main(String[] args) {
+        System.out.println(Arrays.toString(new No1310_xorQueries().xorQueries(new int[]{1, 3, 4, 8}, new int[][]{{0, 1}, {1, 2}, {0, 3}, {3, 3}})));
+    }
+
+    public int[] xorQueries(int[] arr, int[][] queries) {
+        int m = queries.length;
+        int n = arr.length;
+        int[] xor = new int[n + 1];
+        for (int i = 0; i < n; i++) {
+            xor[i + 1] = xor[i] ^ arr[i];
+        }
+        int[] res = new int[m];
+        for (int i = 0; i < m; i++) {
+            int[] query = queries[i];
+            res[i] = xor[query[1] + 1] ^ xor[query[0]];
+        }
+        return res;
+    }
+}
