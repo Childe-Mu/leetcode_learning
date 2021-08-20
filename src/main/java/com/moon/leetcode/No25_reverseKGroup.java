8290c7aa2567a36444b541ebef159f6796b1f521
moon <Childe_Mu@163.com>
2021-08-20 22:05:40 +0800

    25. K 个一组翻转链表
    Signed-off-by: moon <Childe_Mu@163.com>

---
+++ src/main/java/com/moon/leetcode/No25_reverseKGroup.java
@@ -0,0 +1,164 @@
+package com.moon.leetcode;
+
+import java.util.ArrayDeque;
+import java.util.Deque;
+
+// 25. K 个一组翻转链表
+//
+//给你一个链表，每 k 个节点一组进行翻转，请你返回翻转后的链表。
+//
+// k 是一个正整数，它的值小于或等于链表的长度。
+//
+// 如果节点总数不是 k 的整数倍，那么请将最后剩余的节点保持原有顺序。
+//
+// 进阶：
+//
+//
+// 你可以设计一个只使用常数额外空间的算法来解决此问题吗？
+// 你不能只是单纯的改变节点内部的值，而是需要实际进行节点交换。
+//
+//
+//
+//
+// 示例 1：
+//
+//
+//输入：head = [1,2,3,4,5], k = 2
+//输出：[2,1,4,3,5]
+//
+//
+// 示例 2：
+//
+//
+//输入：head = [1,2,3,4,5], k = 3
+//输出：[3,2,1,4,5]
+//
+//
+// 示例 3：
+//
+//
+//输入：head = [1,2,3,4,5], k = 1
+//输出：[1,2,3,4,5]
+//
+//
+// 示例 4：
+//
+//
+//输入：head = [1], k = 1
+//输出：[1]
+//
+//
+//
+//
+//
+// 提示：
+//
+//
+// 列表中节点的数量在范围 sz 内
+// 1 <= sz <= 5000
+// 0 <= Node.val <= 1000
+// 1 <= k <= sz
+//
+// Related Topics 递归 链表
+// 👍 1250 👎 0
+public class No25_reverseKGroup {
+    public static void main(String[] args) {
+        ListNode n = new ListNode(1);
+        n.next = new ListNode(2);
+        n.next.next = new ListNode(3);
+        n.next.next.next = new ListNode(4);
+        n.next.next.next.next = new ListNode(5);
+        new No25_reverseKGroup().reverseKGroup_v2(n, 3);
+
+    }
+
+    public ListNode reverseKGroup_v1(ListNode head, int k) {
+        if (head == null || head.next == null) {
+            return head;
+        }
+        ListNode dummy = new ListNode();
+        ListNode cur = head;
+        ListNode pre = dummy;
+        Deque<ListNode> stack = new ArrayDeque<>();
+        while (cur != null) {
+            stack.offerLast(cur);
+            cur = cur.next;
+            if (!stack.isEmpty() && stack.size() == k) {
+                pre.next = stack.peekLast();
+                pre = stack.peekFirst();
+                pre.next = null;
+                ListNode t1 = stack.pollLast();
+                while (!stack.isEmpty()) {
+                    t1.next = stack.pollLast();
+                    t1 = t1.next;
+                }
+            }
+        }
+        if (!stack.isEmpty()) {
+            pre.next = stack.peekFirst();
+        }
+        return dummy.next;
+    }
+
+    public ListNode reverseKGroup_v2(ListNode head, int k) {
+        ListNode hair = new ListNode(0);
+        hair.next = head;
+        ListNode pre = hair;
+
+        while (head != null) {
+            ListNode tail = pre;
+            // 查看剩余部分长度是否大于等于 k
+            for (int i = 0; i < k; ++i) {
+                tail = tail.next;
+                if (tail == null) {
+                    return hair.next;
+                }
+            }
+            ListNode nex = tail.next;
+            ListNode[] reverse = myReverse(head, tail);
+            head = reverse[0];
+            tail = reverse[1];
+            // 把子链表重新接回原链表
+            pre.next = head;
+            tail.next = nex;
+            pre = tail;
+            head = tail.next;
+        }
+
+        return hair.next;
+    }
+
+    public ListNode[] myReverse(ListNode head, ListNode tail) {
+        ListNode prev = tail.next;
+        ListNode p = head;
+        while (prev != tail) {
+            ListNode nex = p.next;
+            p.next = prev;
+            prev = p;
+            p = nex;
+        }
+        return new ListNode[]{tail, head};
+    }
+
+    private static class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode() {
+        }
+
+        ListNode(int val) {
+            this.val = val;
+        }
+
+        ListNode(int val, ListNode next) {
+            this.val = val;
+            this.next = next;
+        }
+
+        @Override
+        public String toString() {
+            return String.valueOf(val);
+        }
+    }
+}
