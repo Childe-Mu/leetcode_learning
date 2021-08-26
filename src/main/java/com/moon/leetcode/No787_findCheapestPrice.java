1d6eccf84d1a42d6c3d2dd73d4899fcfb0d3dc2a
moon <Childe_Mu@163.com>
2021-08-26 10:10:12 +0800

    787. K 站中转内最便宜的航班
    Signed-off-by: moon <Childe_Mu@163.com>

--- src/main/java/com/moon/leetcode/No787_findCheapestPrice.java
+++ src/main/java/com/moon/leetcode/No787_findCheapestPrice.java
@@ -63,7 +63,8 @@ import java.util.Queue;
 public class No787_findCheapestPrice {
 
     public static void main(String[] args) {
-        System.out.println(new No787_findCheapestPrice().findCheapestPrice(3, new int[][]{{0, 1, 100}, {1, 2, 100}, {0, 2, 500}}, 0, 2, 0));
+        System.out.println(new No787_findCheapestPrice().findCheapestPrice(17, new int[][]{{0, 12, 28}, {5, 6, 39}, {8, 6, 59}, {13, 15, 7}, {13, 12, 38}, {10, 12, 35}, {15, 3, 23}, {7, 11, 26}, {9, 4, 65}, {10, 2, 38}, {4, 7, 7}, {14, 15, 31}, {2, 12, 44}, {8, 10, 34}, {13, 6, 29}, {5, 14, 89}, {11, 16, 13}, {7, 3, 46}, {10, 15, 19}, {12, 4, 58}, {13, 16, 11}, {16, 4, 76}, {2, 0, 12}, {15, 0, 22}, {16, 12, 13}, {7, 1, 29}, {7, 14, 100}, {16, 1, 14}, {9, 6, 74}, {11, 1, 73}, {2, 11, 60}, {10, 11, 85}, {2, 5, 49}, {3, 4, 17}, {4, 9, 77}, {16, 3, 47}, {15, 6, 78}, {14, 1, 90}, {10, 5, 95}, {1, 11, 30}, {11, 0, 37}, {10, 4, 86}, {0, 8, 57}, {6, 14, 68}, {16, 8, 3}, {13, 0, 65}, {2, 13, 6}, {5, 13, 5}, {8, 11, 31}, {6, 10, 20}, {6, 2, 33}, {9, 1, 3}, {14, 9, 58}, {12, 3, 19}, {11, 2, 74}, {12, 14, 48}, {16, 11, 100}, {3, 12, 38}, {12, 13, 77}, {10, 9, 99}, {15, 13, 98}, {15, 12, 71}, {1, 4, 28}, {7, 0, 83}, {3, 5, 100}, {8, 9, 14}, {15, 11, 57}, {3, 6, 65}, {1, 3, 45}, {14, 7, 74}, {2, 10, 39}, {4, 8, 73}, {13, 5, 77}, {10, 0, 43}, {12, 9, 92}, {8, 2, 26}, {1, 7, 7}, {9, 12, 10}, {13, 11, 64}, {8, 13, 80}, {6, 12, 74}, {9, 7, 35}, {0, 15, 48}, {3, 7, 87}, {16, 9, 42}, {5, 16, 64}, {4, 5, 65}, {15, 14, 70}, {12, 0, 13}, {16, 14, 52}, {3, 10, 80}, {14, 11, 85}, {15, 2, 77}, {4, 11, 19}, {2, 7, 49}, {10, 7, 78}, {14, 6, 84}, {13, 7, 50}, {11, 6, 75}, {5, 10, 46}, {13, 8, 43}, {9, 10, 49}, {7, 12, 64}, {0, 10, 76}, {5, 9, 77}, {8, 3, 28}, {11, 9, 28}, {12, 16, 87}, {12, 6, 24}, {9, 15, 94}, {5, 7, 77}, {4, 10, 18}, {7, 2, 11}, {9, 5, 41}},
+                13, 4, 13));
     }
 
     /**
@@ -91,8 +92,10 @@ public class No787_findCheapestPrice {
                     continue;
                 }
                 for (int[] p : list) {
-                    queue.offer(new int[]{p[1], poll[1] + p[2]});
-                    dist[p[1]] = Math.min(dist[p[1]], poll[1] + p[2]);
+                    if (dist[p[1]] > poll[1] + p[2]) {
+                        queue.offer(new int[]{p[1], poll[1] + p[2]});
+                        dist[p[1]] = poll[1] + p[2];
+                    }
                 }
             }
         }
@@ -131,5 +134,41 @@ public class No787_findCheapestPrice {
             }
         }
         return dist[dst] == Integer.MAX_VALUE ? -1 : dist[dst];
+    }
+
+    public int findCheapestPrice_v4(int n, int[][] flights, int src, int dst, int k) {
+        int[] dis = new int[n];
+        Arrays.fill(dis, -1);
+        dis[src] = 0;
+        List<int[]>[] adj = new ArrayList[n];
+        for (int[] flight : flights) {
+            if (adj[flight[0]] == null) {
+                adj[flight[0]] = new ArrayList<>();
+            }
+            adj[flight[0]].add(flight);
+        }
+        Queue<int[]> queue = new LinkedList<>();
+        if (adj[src] == null) return -1;
+        for (int[] f : adj[src]) {
+            int[] x = new int[]{f[0], f[1], f[2], 0};
+            queue.offer(x);
+        }
+        while (!queue.isEmpty()) {
+            int[] x = queue.poll();
+            if (dis[x[1]] != -1 && dis[x[1]] < x[2]) {
+                continue;
+            }
+            dis[x[1]] = x[2];
+            if (adj[x[1]] != null && x[3] < k) {
+                for (int[] f : adj[x[1]]) {
+                    int[] y = new int[]{f[0], f[1], x[2] + f[2], x[3] + 1};
+                    if (dis[y[1]] != -1 && dis[y[1]] < y[2]) {
+                        continue;
+                    }
+                    queue.offer(y);
+                }
+            }
+        }
+        return dis[dst];
     }
 }
