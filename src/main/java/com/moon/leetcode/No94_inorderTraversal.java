1383ae1f5ce5d1ebd1f9d3a5a246eb4c11d08728
moon <childe_mu@163.com>
2021-11-18 22:17:39 +0800

    94. 二叉树的中序遍历

---
+++ src/main/java/com/moon/leetcode/No94_inorderTraversal.java
@@ -0,0 +1,96 @@
+package com.moon.leetcode;
+
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.Deque;
+import java.util.List;
+
+// 94. 二叉树的中序遍历
+//给定一个二叉树的根节点 root ，返回它的 中序 遍历。
+//
+//
+//
+// 示例 1：
+//
+//
+//输入：root = [1,null,2,3]
+//输出：[1,3,2]
+//
+//
+// 示例 2：
+//
+//
+//输入：root = []
+//输出：[]
+//
+//
+// 示例 3：
+//
+//
+//输入：root = [1]
+//输出：[1]
+//
+//
+// 示例 4：
+//
+//
+//输入：root = [1,2]
+//输出：[2,1]
+//
+//
+// 示例 5：
+//
+//
+//输入：root = [1,null,2]
+//输出：[1,2]
+//
+//
+//
+//
+// 提示：
+//
+//
+// 树中节点数目在范围 [0, 100] 内
+// -100 <= Node.val <= 100
+//
+//
+//
+//
+// 进阶: 递归算法很简单，你可以通过迭代算法完成吗？
+// Related Topics 栈 树 深度优先搜索 二叉树
+// 👍 1166 👎 0
+public class No94_inorderTraversal {
+    public List<Integer> inorderTraversal(TreeNode root) {
+        List<Integer> res = new ArrayList<>();
+        Deque<TreeNode> stack = new ArrayDeque<>();
+        while (root != null || !stack.isEmpty()) {
+            while (root != null) {
+                stack.push(root);
+                root = root.left;
+            }
+            root = stack.pop();
+            res.add(root.val);
+            root = root.right;
+        }
+        return res;
+    }
+
+    private static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+}
