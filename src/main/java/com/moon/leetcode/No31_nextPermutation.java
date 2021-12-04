ceeea52da16a86808b383208bfd20b92650d84a1
moon <childe_mu@163.com>
2021-12-04 22:16:03 +0800

    31. 下一个排列

---
+++ src/main/java/com/moon/leetcode/No31_nextPermutation.java
@@ -0,0 +1,89 @@
+package com.moon.leetcode;
+
+import java.util.Arrays;
+
+// 31. 下一个排列
+//实现获取 下一个排列 的函数，算法需要将给定数字序列重新排列成字典序中下一个更大的排列（即，组合出下一个更大的整数）。
+//
+// 如果不存在下一个更大的排列，则将数字重新排列成最小的排列（即升序排列）。
+//
+// 必须 原地 修改，只允许使用额外常数空间。
+//
+//
+//
+// 示例 1：
+//
+//
+//输入：nums = [1,2,3]
+//输出：[1,3,2]
+//
+//
+// 示例 2：
+//
+//
+//输入：nums = [3,2,1]
+//输出：[1,2,3]
+//
+//
+// 示例 3：
+//
+//
+//输入：nums = [1,1,5]
+//输出：[1,5,1]
+//
+//
+// 示例 4：
+//
+//
+//输入：nums = [1]
+//输出：[1]
+//
+//
+//
+//
+// 提示：
+//
+//
+// 1 <= nums.length <= 100
+// 0 <= nums[i] <= 100
+//
+// Related Topics 数组 双指针
+// 👍 1433 👎 0
+public class No31_nextPermutation {
+
+    public static void main(String[] args) {
+        int[] a = {1, 3, 2};
+        new No31_nextPermutation().nextPermutation(a);
+        System.out.println(Arrays.toString(a));
+    }
+
+    public void nextPermutation(int[] nums) {
+        int n = nums.length;
+        int i = n - 2;
+        while (i >= 0 && nums[i] >= nums[i + 1]) {
+            i--;
+        }
+        if (i >= 0) {
+            int j = n - 1;
+            while (j > i && nums[i] >= nums[j]) {
+                j--;
+            }
+            swap(nums, i, j);
+        }
+        reverse(nums, i + 1, n - 1);
+    }
+
+    private void swap(int[] nums, int i, int j) {
+        int t = nums[i];
+        nums[i] = nums[j];
+        nums[j] = t;
+    }
+
+    private void reverse(int[] nums, int l, int r) {
+        while (l < r) {
+            int t = nums[l];
+            nums[l++] = nums[r];
+            nums[r--] = t;
+        }
+    }
+}
